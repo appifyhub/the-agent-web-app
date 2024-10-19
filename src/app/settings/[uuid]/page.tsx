9abf14fa82cda9ee9e8e3fad1658a92f0f8b7e41
Milos <milos192@users.noreply.github.com>
2024-10-19 21:36:36 +0200

    Add a /settings page with a sample Input OTP

---
+++ src/app/settings/[uuid]/page.tsx
@@ -0,0 +1,27 @@
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "../../../components/ui/input-otp";
+
+export default function Settings({ params }: { params: { uuid: string } }) {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Welcome to the settings page, {params.uuid}. Please authenticate first:
+      <InputOTP maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+    </div>
+  );
+}
