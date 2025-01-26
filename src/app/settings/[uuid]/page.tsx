3e08ce245e065780b901a743df2a22241d0e6e60
Milos Marinkovic <milosmns@gmail.com>
2025-01-26 23:21:29 +0100

    UI Facelift to match the new dependencies

--- src/app/settings/[uuid]/page.tsx
+++ src/app/settings/[uuid]/page.tsx
@@ -5,10 +5,15 @@ import {
   InputOTPSlot,
 } from "../../../components/ui/input-otp";
 
-export default function Settings({ params }: { params: { uuid: string } }) {
+export default async function Settings({
+  params,
+}: {
+  params: { uuid: string };
+}) {
+  const { uuid } = params;
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcome to the settings page, {params.uuid}. Please authenticate first:
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      Welcome to the settings page, {uuid}. Please authenticate first:
       <InputOTP maxLength={6}>
         <InputOTPGroup>
           <InputOTPSlot index={0} />
