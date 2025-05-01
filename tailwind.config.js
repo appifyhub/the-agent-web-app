72c0095e5a037795ade10db4664ecb84d9e3f9a8
Milos Marinkovic <milosmns@gmail.com>
2025-05-01 03:56:36 +0200

    Add favicon and dark styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontSize: {
+        base: "1.2rem",
+        lg: "1.25rem",
+        xl: "1.5rem",
+      },
+    },
+  },
   plugins: [],
 };
