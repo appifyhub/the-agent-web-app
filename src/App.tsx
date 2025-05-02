c3dde3c09a6b7f589fca5f34941531bd27568c99
Milos Marinkovic <milosmns@gmail.com>
2025-05-02 22:40:02 +0200

    Clean up and standardize root CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import ChatSettingsPage from "./pages/ChatSettingsPage";
 
 function App() {
