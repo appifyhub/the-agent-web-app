88579628b25adc3e4308569dfbdf36b7bf4a143e
Milos Marinkovic <milosmns@gmail.com>
2025-07-26 19:15:21 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1024,
+  },
   server: {
     allowedHosts: true,
   },
